a0aeb73d511d7a5987165e1ea6c670fe7db2eedf
AntonIrkut <anton.sokoloff2016@yandex.ru>
2022-09-08 21:23:14 +0800

    07destructuring

---
+++ src/07destructing/destructing.test.ts
@@ -0,0 +1,46 @@
+import {ManType} from "./Destructing";
+
+
+let props: ManType;
+beforeEach(() => {
+    props = {
+        name: "Anton",
+        age: 32,
+        lessons: [{title: "1"}, {title: "2"}, {title: "3"}],
+        address: {
+            street: {
+                title: 'Decabryskih street'
+            }
+        }
+    }
+})
+
+test("", () => {
+    // const age = props.age;
+    // const lessons = props.lessons;
+    const {age, lessons} = props;
+    const {title} = props.address.street
+
+
+    expect(age).toBe(32);
+    expect(lessons.length).toBe(3);
+
+
+    expect(title).toBe('Decabryskih street')
+})
+
+test("", () => {
+    const l1 = props.lessons[0];
+    const l2 = props.lessons[1];
+
+    const [ls1, ...restLessons] = props.lessons;
+
+    expect(l1.title).toBe('1')
+    expect(l2.title).toBe('2')
+
+    expect(ls1.title).toBe('1')
+    // expect(ls2.title).toBe('2')
+
+    expect(restLessons.length).toBe(2)
+    expect(restLessons[0].title).toBe('2')
+})
